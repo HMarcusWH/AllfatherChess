22d5dcb0231b6f900996e22a1a9f7977a9893870
Marcus Hermansson <marcus@hmwh.se>
2026-09-23 22:59:18 +0200

    M14-C: expose unsettled EXPLORE reservations to authority gate

--- controller/routing.py
+++ controller/routing.py
@@ -560,6 +560,9 @@ class ConservativeRouter:
             ),
             "specialist_settlement_complete": not self._specialist_unresolved,
             "open_specialist_reservations": len(self._specialist_reservations),
+            "open_solver_reservations": sum(
+                len(reservations) for reservations in self._reservations.values()
+            ),
             "gpu_accounted": self._gpu_accounted(),
             "controller_fallback_latched": bool(self._fallback),
         }
